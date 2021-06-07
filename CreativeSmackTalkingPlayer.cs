252744d8b983b5f81636e92a62ff21b426bb591d
Logan Demmy <frank.demmy@gmail.com>
2021-06-07 13:44:35 -0500

    creative smack talking player

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,27 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
+        
+        public List<string> Taunts { get; set; }
 
+       public CreativeSmackTalkingPlayer(){
+           Taunts = new List<string>();
+       }
+        
+         public override int Roll()
+        {
+            
+            Random i = new Random();
+        int genRand = i.Next(0, Taunts.Count - 1);
+            // Return a random number between 1 and DiceSize
+            Console.WriteLine($"Smack talking play shouts a taunt... {Taunts[genRand]}");
+            return new Random().Next(DiceSize) + 1;
+        }
+        
     }
 }
